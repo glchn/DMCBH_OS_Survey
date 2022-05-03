c3c2b04e3ad2e263f7a251facc8547ece7c6eb06
Gale Chen <61856642+glchn@users.noreply.github.com>
2022-05-03 14:45:59 -0700

    Plot formatting
    Don't know how to define bar size in an absolute manner...

--- os_survey_plots.R
+++ os_survey_plots.R
@@ -1,4 +1,5 @@
 # assumes you downloaded your data from the qualtrics results tables in .csv file...
+# TODO: absolute size for bars
 
 # variables you need to update before running script ---------------------------------
 subdir <- "/22s_co-op/os_qualtrics_survey_20220502"
@@ -7,9 +8,10 @@ q52_csv <- "q52_table.csv"
 q56_csv <- "q56_table.csv"
 q57_csv <- "q57_table.csv"
 q58_csv <- "q58_table.csv"
-text_size = 14
-subtitle_size=15
-title_size=17
+text_size = 15
+subtitle_size = 15
+title_size = 17
+bar_width = 0.5
 
 #set up directory!--------------------------------------------------------------------
 data_dir <- paste0( Sys.getenv("HOME"), subdir)
@@ -23,6 +25,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(scales)
+library(gridExtra)
 
 # plop in q55 -----------------------------------------------------------------------
 q55 <- read.csv(q55_csv)
@@ -32,18 +35,21 @@ q55_cleaned <- q55_cleaned[-c(6), ]
 q55_cleaned[q55_cleaned == "Totally Diagree"] <- "Totally Disagree" # fixing a typo = =
 
 # make plot for q55, uses counts (not percentages)
-png('q55_counts.png', width = 900,height = 700)
-# TODO there has to be a smarter way of doing this... couldn't figure this out 
+png('q55_plot.png', width = 900,height = 700)
 # (see previous commits): https://r-graph-gallery.com/267-reorder-a-variable-in-ggplot2.html
 q55_cleaned$Answer <- factor(q55_cleaned$Answer, levels=c("Totally Agree", "Agree", "Neutral", "Disagree", "Totally Disagree"))
   ggplot(q55_cleaned, aes(x=Answer, y=Count)) +
     ggtitle("Q55 - DMCBH becoming an OSI would bring value to the DMCBH community.") + 
-    geom_bar(stat = "identity", fill = "#6ca8d9") +
-    xlab("Response") +
-    ylab("Count") + 
+    geom_bar(stat = "identity", fill = "#6ca8d9", width = 0.7) +
+    xlab("") +
+    ylab("Number of Responses") +
     theme(title=element_text(size=title_size, face="bold"),
-          axis.title=element_text(size=subtitle_size,face="bold"),
-          axis.text=element_text(size=text_size))
+          axis.title=element_text(size=subtitle_size, face="bold"),
+          axis.text=element_text(size=text_size, face="bold"),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(), 
+          axis.line = element_line(colour = "black"))
 dev.off()
 
 # plot q52 and 56 ---------------------------------------------------------------------
@@ -56,33 +62,41 @@ q52_cleaned <- q52[,-1]
 q52_cleaned <- q52_cleaned[-c(4), ]
 q52_cleaned$Answer<-gsub("\\.","",as.character(q52_cleaned$Answer)) # note that . by itself just means any character
 
-png('q52_counts.png', width = 1000,height = 700)
+png('q52_plot.png', width = 1000,height = 700)
 q52_cleaned$Answer <- factor(q52_cleaned$Answer, levels=c("Yes", "No", "I don't know yet"))
 ggplot(q52_cleaned, aes(x=Answer, y=Count)) +
   ggtitle("Q52 - Based on the information available to you and your current understanding of what an OSI 
           is, should we position the DMCBH as an OSI?") + 
-  geom_bar(stat = "identity", fill = "#6ca8d9") +
-  xlab("Response") +
-  ylab("Count") +
+  geom_bar(stat = "identity", fill = "#6ca8d9", width = 0.385) +
+  xlab("") +
+  ylab("Number of Responses") +
   theme(title=element_text(size=title_size, face="bold"),
         axis.title=element_text(size=subtitle_size,face="bold"),
-        axis.text=element_text(size=text_size))
+        axis.text=element_text(size=text_size, face="bold"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
 dev.off()
 
 q56 <- read.csv(q56_csv)
 colnames(q56)[3] <- "Response rate (%)"
 q56_cleaned <- q56[,-1]
 q56_cleaned <- q56_cleaned[-c(3), ]
-png('q56_counts.png', width = 920,height = 700)
+png('q56_plot.png', width = 920,height = 700)
 q56_cleaned$Answer <- factor(q56_cleaned$Answer, levels=c("Yes", "No"))
 ggplot(q56_cleaned, aes(x=Answer, y=Count)) +
   ggtitle("Q56 - Do you think OS is especially important in neuroscience as compared to other fields?") + 
-  geom_bar(stat = "identity", fill = "#6ca8d9") +
-  xlab("Response") +
-  ylab("Count") +
+  geom_bar(stat = "identity", fill = "#6ca8d9", width = 0.29) +
+  xlab("") +
+  ylab("Number of Responses") +
   theme(title=element_text(size=title_size, face="bold"),
         axis.title=element_text(size=subtitle_size,face="bold"),
-        axis.text=element_text(size=text_size))
+        axis.text=element_text(size=text_size, face="bold"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
 dev.off()
 
 # plot 57,58 --------------------------------------------------------------------------
@@ -114,7 +128,7 @@ total$Answer[total$Answer=="Science benefits everyone regardless of cultural vie
 total <- total[-c(4,8),]
 
 # attempting grouped... not working :-(
-png('q5758_counts.png', width = 1000,height = 700)
+png('q5758_plot.png', width = 1100,height = 700)
 total$Answer <- factor(total$Answer, levels=c("High burden of disease",
                                               "Complexity of the brain",
                                               "Respect for cultural diversity",
@@ -125,9 +139,9 @@ ggplot(total, aes(x=Answer, y=Count, fill=str_wrap(Prev, 30))) +
   labs(fill="Answer to Q56") +
   ggtitle("Q57/58 - Opinions on the importance of OSI in neuroscience") +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-  geom_bar(position="dodge", stat="identity") +
-  xlab("Response") +
-  ylab("Count") +
+  geom_bar(position="dodge", stat="identity", width = 0.68) +
+  xlab("") +
+  ylab("Number of Responses") +
   scale_fill_manual(values=c("#6ca8d9",
                              "#f58070")) +
   scale_x_discrete(labels = wrap_format(35)) +
@@ -136,5 +150,10 @@ ggplot(total, aes(x=Answer, y=Count, fill=str_wrap(Prev, 30))) +
         legend.title=element_text(size=subtitle_size, face="bold"),
         title=element_text(size=title_size, face="bold"),
         axis.title=element_text(size=subtitle_size,face="bold"),
-        axis.text=element_text(size=text_size))
+        axis.text=element_text(size=text_size, face="bold"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        legend.position = c(0.85, 0.85))
 dev.off()
